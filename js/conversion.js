a6769be8fb455b4d2af74333e9b11c7410332ef8
harrygfox <henry.george.fox@gmail.com>
2015-03-13 15:45:01 +0000

    creating new canvas for experiementation

---
+++ js/conversion.js
@@ -0,0 +1,2 @@
+// implement variations on var notes and pass those values through a convertor.
+// The convertor will change the format and pass the vlaue to var main. 
---
+++ js/stave.js
@@ -0,0 +1,3 @@
+var canvas = $('.canvas-wrapper-2 canvas')[0],
+    renderer = new Vex.Flow.Renderer(canvas, Vex.Flow.Renderer.Backends.CANVAS),
+    ctx = renderer.getContext();
